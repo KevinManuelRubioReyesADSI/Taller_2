e27dbdbeace9eaedd106be7a5814f81c71cde7dc
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 08:34:40 -0500

    punto_1

---
+++ main.js
@@ -0,0 +1,8 @@
+let num = Number(prompt('Digite un numero '));
+if(num>0 && num<100){
+    document.write("<p class='resultado'>EL numero es positivo y menor que 100"+"</p>");
+}
+else{
+    document.write("<p class='resultado'>El numero es negativo"+"</p>");
+}
+
