eb5c76c8250eb517392b2cc339c33ccc3d18e7bd
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 11:18:41 -0500

    punto_5

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-let elegirFigura = prompt("A que figura le desea calcular el área:" + "\n" +
-    "¿Triangulo o circulo" + "\n" + 
-    "Escribe T para Triangulo" + "\n" +
-    "Escriba C para circulo");
-    if (elegirFigura === "T"){
-        let base = Número (prompt("Ingrese la base del triangulo "));
-        let altura = Número (prompt("Ingrese la altura del triangulo"));
-        let areaTr = base * altura / 2 ;
-        alert ("El area del triangulo es de " + areaTr);
-    }
-    else if (elefirfigu == "c"){
-        let radio = Número(prompt("Ingrese el radio del circulo"));
-         areaCir = Math.PI * radio;
-        alert("El area del circulo es de " + areaCir)
-    }
+let lado1 = Número(prompt("Ingrese el primer lado"));
+let lado2 = Número(prompt("Ingrese el lado dos"));
+si ( lado1 > 0 && lado2 > 0);
+{
+    let area = lado1 * lado2;
+    alert (" EL area del rectangulo es de " + area);
+}
+
+{
+    alert ("Si quiere sacar el área los lados deben ser positivos ");
+}
