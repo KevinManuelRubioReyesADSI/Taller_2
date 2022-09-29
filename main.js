a9b1dee4c34e39cb90fb333b0d72e44fc6ff9f8d
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 10:15:57 -0500

    punto_3

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-let numero1 = (prompt('Ingrese el primer numero'));
-let numero2 = (prompt('Ingrese el segundo numero'));
-let numero3 = (prompt('Ingrese el tercer numero'));
-if (num1 > numero2 && numero1 > numero3);{
-    documento_escribir('El numero mayor es ' + num1);
-}any (num2 > numero3 && numero2 > numero1);{
-    documento_escribir('EL numero mayor es ' + num2);
+
+document.querySelector("#mayor").innerHTML="";
+document.querySelector("#menor").innerHTML="";
+document.querySelector("#iguales").innerHTML="";
+
+if (num1>=num2 && num2>=num3){
+    document.querySelector("#mayor").innerHTML= "El numero mayor es:"+num1;
+    if (num2>=num3){
+        document.querySelector("#menor").innerHTML="EL numeor menor es:"+num2;
+    }else{
+        document.querySelector("#menor").innerHTML="El numero menor es:"+num3;
+    }
+} else if (num2>num1 && num2>=num3){
+    document.querySelector("#mayor").innerHTML="El numeor mayor es:"+num2;
+    if(num1>num3){
+        document.querySelector("#mayor").innerHTML="El numero mayor es:"+num3;
+    } else{
+        document.querySelector("#menor").innerHTML="El numero menor es:"+num1;
+    }
+} else if (num3>num1 && num3>=num2){
+    document.querySelector("#mayor").innerHTML="El numero mayor es:" +num3;
+    if(num1>num2){
+        document.querySelector("#mayor").innerHTML="El numero mayor es:"+num2;
+    }else{
+        document.querySelector("#menor").innerHTML="El numero menor es:"+num1
+    }
+}else{
+    document.querySelector("#iguales").innerHTML="Todos los numeros son iguales"+num1
 }
-{
-    docuemt_escribir('El numero mayor es ' + num3)
-}
+
+addEventListener("DOMContentLoaded", (e)=>{
+    let cal= document.querySelector('#form');
+    let num1= document.querySelector("#num1").value;
+    let num2= document.querySelector("#num2").value;
+    let num3= document.querySelector("#num3").value;
+    let resultado= mayor(num1, num2, num3);
+    document.querySelector("#res").innerHTML=resultado
+    mayor(num1, num2, num3);
+})
