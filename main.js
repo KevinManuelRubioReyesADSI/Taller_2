9711a76e28efc5f266c8c73a96739036a9e91566
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 11:35:18 -0500

    punto_6

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let lado1 = Número(prompt("Ingrese el primer lado"));
-let lado2 = Número(prompt("Ingrese el lado dos"));
-si ( lado1 > 0 && lado2 > 0);
-{
-    let area = lado1 * lado2;
-    alert (" EL area del rectangulo es de " + area);
+let temperatura = Número(prompt("Ingrese su temperatura"));
+let presion = Número (prompt("Ingrese su presión"));
+if(temperatura > 200 && presion > 100){
+    alert ("alarma");
 }
-
-{
-    alert ("Si quiere sacar el área los lados deben ser positivos ");
+mas{
+    alert("Normal");
 }
