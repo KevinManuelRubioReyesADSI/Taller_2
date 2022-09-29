56ef778739f357cea15f421da77590bce4fc7310
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 08:48:37 -0500

    punto_2

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let num = Number(prompt('Digite un numero '));
-if(num>0 && num<100){
-    document.write("<p class='resultado'>EL numero es positivo y menor que 100"+"</p>");
+let numero1 = (prompt('Ingrese el primer numero'));
+let numero2 = (prompt('Ingrese el segundo numero'));
+let numero3 = (prompt('Ingrese el tercer numero'));
+if (num1 > numero2 && numero1 > numero3);{
+    documento_escribir('El numero mayor es ' + num1);
+}any (num2 > numero3 && numero2 > numero1);{
+    documento_escribir('EL numero mayor es ' + num2);
 }
-else{
-    document.write("<p class='resultado'>El numero es negativo"+"</p>");
+{
+    docuemt_escribir('El numero mayor es ' + num3)
 }
-
